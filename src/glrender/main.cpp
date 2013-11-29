9ad349b84ab9fef1ad46a452c775a284c6fbd151
Andreas Valter <andreasvalter@gmail.com>
2013-11-29 21:46:59 +0000

    Managed to get glfw to work aswell.

--- src/glrender/main.cpp
+++ src/glrender/main.cpp
@@ -1,40 +1,39 @@
-//#include <GL/glew.h>
-
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <stdlib.h> // Exit and status
 
-//static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-//{
-//    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-//        glfwSetWindowShouldClose(window, GL_TRUE);
-//}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main(int argc, char **argv) {
-	std::cout << "hej\n";
-	//if (!glfwInit()) {
-	//	exit(EXIT_FAILURE);
-	//}
-	//
-	//GLFWwindow* window = glfwCreateWindow(640, 480, "Cumulus", NULL, NULL);
-	//
-	//glfwSetKeyCallback(window, key_callback);
-	//
-	//if (!window) {
-	//	glfwTerminate();
-	//	exit(EXIT_FAILURE);
-	//}
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+	
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Cumulus", NULL, NULL);
+	
+	glfwSetKeyCallback(window, key_callback);
+	
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
 
-	//glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
 
-	//while (!glfwWindowShouldClose(window))
-	//{
- //   	// Keep running
- //   	glfwSwapBuffers(window);
- //       glfwPollEvents();
-	//}
+	while (!glfwWindowShouldClose(window))
+	{
+    	// Keep running
+    	glfwSwapBuffers(window);
+        glfwPollEvents();
+	}
 
-	//glfwDestroyWindow(window);
-	//glfwTerminate();
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
