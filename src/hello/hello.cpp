ddfcd016c6f183a344586c1706c26aa44b7973e7
Andreas Valter <andreasvalter@gmail.com>
2013-12-02 18:14:54 +0000

    Added node class. Takes in float, and returns same float.

--- src/hello/hello.cpp
+++ src/hello/hello.cpp
@@ -1,9 +1,38 @@
-#include <maya/MSimple.h>
+#include <maya/MPxNode.h>
 #include <maya/MIOStream.h>
+#include <maya/MString.h>
+#include <maya/MTypeId.h>
+#include <maya/MPlug.h>
+#include <maya/MDataBlock.h>
+#include <maya/MDataHandle.h> 
+#include <maya/MFnNumericAttribute.h>
+#include <maya/MFnMatrixAttribute.h>
+#include <maya/MFloatVector.h>
+#include <maya/MFloatPoint.h>
+#include <maya/MFnPlugin.h>
 
-DeclareSimpleCommand( helloWorld, "Autodesk", "2014");
-MStatus helloWorld::doIt( const MArgList& )
+class Cloud : public MPxNode 
 {
-    cout << "Hello World\n" << endl;
-    return MS::kSuccess;
-}
+public:
+	Cloud();
+    virtual ~Cloud();
+
+	virtual MStatus compute( const MPlug&, MDataBlock& );
+virtual void    postConstructor();
+
+	static  void *  creator();
+	static  MStatus initialize();
+
+	//  Id tag for use with binary file format
+	static  MTypeId id;
+};
+
+// Static data
+MTypeId Cloud::id(0x81016);
+
+Cloud::Cloud() { };
+
+MStatus compute(const MPlug &plug, MDataBlock &block) { 
+	return MS::kSuccess;
+};
+
---
+++ src/mayarender/cloud_node.h
@@ -0,0 +1,46 @@
+#ifndef _buildMayaNode
+#define _buildMayaNode
+//
+// Copyright (C) 
+// 
+// File: buildMayaNode.h
+//
+// Dependency Graph Node: buildMaya
+//
+// Author: Maya Plug-in Wizard 2.0
+//
+
+#include <maya/MPxNode.h>
+#include <maya/MFnNumericAttribute.h>
+#include <maya/MTypeId.h> 
+
+ 
+class CloudNode : public MPxNode
+{
+public:
+						CloudNode();
+	virtual				~CloudNode(); 
+
+	virtual MStatus		compute( const MPlug& plug, MDataBlock& data );
+
+	static  void*		creator();
+	static  MStatus		initialize();
+
+public:
+
+	// There needs to be a MObject handle declared for each attribute that
+	// the node will have.  These handles are needed for getting and setting
+	// the values later.
+	//
+	static  MObject		input;		// Example input attribute
+	static  MObject		output;		// Example output attribute
+
+
+	// The typeid is a unique 32bit indentifier that describes this node.
+	// It is used to save and retrieve nodes of this type from the binary
+	// file format.  If it is not unique, it will cause file IO problems.
+	//
+	static	MTypeId		id;
+};
+
+#endif
