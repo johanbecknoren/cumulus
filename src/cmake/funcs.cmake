631fd3261ef4a0fe31f2a3c418a9c5f3e22482b9
Andreas Valter <andreasvalter@gmail.com>
2013-12-06 12:58:38 +0100

    Core is a shared library that glrenderer uses now!

--- src/cmake/funcs.cmake
+++
@@ -1,4 +0,0 @@
-macro(f_create_glfw_executable project libs src headers)
-  add_executable(${project} ${src} ${headers})
-  target_link_libraries(${project} ${libs} GL X11 m Xxf86vm Xrandr pthread Xi)
-endmacro()
---
+++ src/core/coregl.cpp
@@ -0,0 +1,9 @@
+#include "coregl.h"
+#include <iostream>
+core::CoreGL::CoreGL() {
+	te();
+}
+
+void core::CoreGL::te() {
+	std::cout << "testing testing" << std::endl;
+}
